e347234bd44d4ab0b58e10918d8902db0584d1f1
Valere Dumont <dumontvalere.vrd@gmail.com>
2022-12-17 14:50:00 +0100

    creation of an grid class

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,61 +3,34 @@
 Sudoku::Sudoku()
     : _difficulty(1), _size(3)
 {
-    _grid.resize(pow(_size, 4), 0); // creartion of an grid of 'size' by 'size' squares with an dimention of 'size' by 'size'
+    Grid _grid(3); // creartion of an grid of 'size' by 'size' squares with an dimention of 'size' by 'size'
 }
 
 Sudoku::Sudoku(unsigned int difficulty)
     : _difficulty(difficulty), _size(3)
 {
-    _grid.resize(pow(_size, 4));
+    Grid _grid(_size); // creartion of an grid of 'size' by 'size' squares with an dimention of 'size' by 'size'
 }
 
 Sudoku::Sudoku(unsigned int difficulty, unsigned int size)
     : _difficulty(difficulty), _size(size)
 {
-    _grid.resize(pow(_size, 4));
+    Grid _grid(_size);
 }
 
 unsigned int Sudoku::getCase(unsigned int x, unsigned int y) const
 {
-    return _grid.at(y * _size * _size + x); // return the case using an x,y coordonate system instead of an all the number in line one
+    return _grid.getCase(x, y); // return the case using an x,y coordonate system instead of an all the number in line one
 }
 
 void Sudoku::setCase(unsigned int x, unsigned int y, unsigned int value)
 {
-    _grid.at(y * _size * _size + x) = value; // set the case using an x,y coordonate system instead of an all the number in line one
+    _grid.setCase(x, y, value); // set the case using an x,y coordonate system instead of an all the number in line one
 }
 
-// TODO : manage the high number (2 ans more digits) for the grid with size > 3
 std::ostream &operator<<(std::ostream &os, const Sudoku &sudoku) // << overload to print the grid
 {
-    for (unsigned int i = 0; i < 2 * (sudoku._size * sudoku._size + sudoku._size) + 1; i++)
-        os << "-";
-    os << std::endl;
-    for (unsigned int y = 0; y < sudoku._size * sudoku._size; y++)
-    {
-        os << "| ";
-        for (unsigned int x = 0; x < sudoku._size * sudoku._size; x++)
-        {
-            if (x % sudoku._size == 0 && x != 0)
-                os << "| ";
-
-            int caseValue = sudoku.getCase(x, y);
-            caseValue == 0 ? os << "." : os << caseValue;
-            caseValue >= 10 ? os << "" : os << " ";
-        }
-        os << "|";
-        os << std::endl;
-        if ((y + 1) % sudoku._size == 0 && y != 0 && y != sudoku._size * sudoku._size - 1)
-        {
-            for (unsigned int i = 0; i < 2 * (sudoku._size * sudoku._size + sudoku._size) + 1; i++)
-                os << "-";
-            os << std::endl;
-        }
-    }
-    for (unsigned int i = 0; i < 2 * (sudoku._size * sudoku._size + sudoku._size) + 1; i++)
-        os << "-";
-    os << std::endl;
+    std::cout << sudoku._grid << std::endl;
     return os;
 }
 
@@ -94,33 +67,11 @@ bool Sudoku::caseIsCorrect(unsigned int x, unsigned int y, unsigned int value) c
     // if the 3 sudoku rules are respected and the case is emplty we can add the case here
 }
 
-void Sudoku::generateEmptyGridWithCasefillInItToPlatTheGameSoItsNotReallyEmpltyWithAnNonWorkingWay() // ! DO NOT USE, WORK BUT ONLY WHEN THE MOON, THE SUN AND JUPITER ARE ALIGNED and dont do exacly what I want (╯’□’)╯︵ ┻━┻ ....
-{
-    srand(time(nullptr)); // initialisation of the randomizer
-    std::array<int, 6> possibleValue = {50, 45, 40, 35, 30, 25};
-    if (_difficulty > 6)
-        _difficulty = 6;
-    if (_difficulty < 1)
-        _difficulty = 1;
-    unsigned int amountToGenerate = possibleValue.at(_difficulty - 1);
-
-    for (unsigned int i = 0; i < amountToGenerate; i++)
-    {
-        unsigned int x = 0, y = 0, value = 0;
-        do
-        {
-            x = rand() % (_size * _size);
-            y = rand() % (_size * _size);
-            value = rand() % (_size * _size) + 1;
-        } while (!caseIsCorrect(x, y, value));
-        setCase(x, y, value);
-    }
-}
-
 void Sudoku::genrateStartingGrid()
 {
     srand(time(NULL)); // initialisation of the randomizer
 
+    // create a valid grid
     int i = 0;
     for (int n = 0; n < pow(_size, 4); n++)
     {
@@ -128,21 +79,23 @@ void Sudoku::genrateStartingGrid()
             i += _size;
         if (n % (_size*_size*_size) == 0 && n != 0)
             i++;
-        _grid.at(n) = n + i;
+        _grid.getGrid().at(n) = n + i;
     }
 
     for (int n = 0; n < pow(_size, 4); n++)
     {
-        _grid.at(n) = _grid.at(n) % (_size * _size) + 1;
+        _grid.getGrid().at(n) = _grid.getGrid().at(n) % (_size * _size) + 1;
     }
 
-    //remove random numbers to match the difficulty
+    shuffleGrid();
+
+    // remove random numbers to match the difficulty
     std::array<int, 6> possibleValue = {50, 45, 40, 35, 30, 25};
     if (_difficulty > 6)
         _difficulty = 6;
     if (_difficulty < 1)
         _difficulty = 1;
-    unsigned int amountToGenerate = possibleValue.at(_difficulty - 1);
+    unsigned int amountToGenerate = possibleValue.at(_difficulty - 1)*pow(_size/3, 3);
     for (unsigned int i = 0; i < pow(_size,4) - amountToGenerate; i++)
     {
         unsigned int x = 0, y = 0;
@@ -156,39 +109,33 @@ void Sudoku::genrateStartingGrid()
     }
 }
 
-bool Sudoku::solve() //*https://en.wikipedia.org/wiki/Backtracking
-{
-    bool solved = false;
-
-    return solved;
-}
-
-void Sudoku::playTheGame()
+void Sudoku::playTheGame() //! not usefull and coded awfully but it's fun
 {
     int x, y, value;
     do
     {
-        std::cout << std::endl
-                  << *this << std::endl;
+        std::cout << std::endl<< *this << std::endl;
         do
         {
-            std::cout << "Enter x: ";
-            std::cin >> x;
-            std::cout << std::endl;
-        } while (!(x >= 0 && x < 9));
-        do
-        {
-            std::cout << "Enter y: ";
-            std::cin >> y;
-            std::cout << std::endl;
-        } while (!(y >= 0 && y < 9));
-        do
-        {
-            std::cout << "Enter value: ";
-            std::cin >> value;
-            std::cout << std::endl;
-        } while (!(value >= 0 && value < 9));
-
+            do
+            {
+                std::cout << "Enter x: ";
+                std::cin >> x;
+                std::cout << std::endl;
+            } while (!(x >= 0 && x < 9));
+            do
+            {
+                std::cout << "Enter y: ";
+                std::cin >> y;
+                std::cout << std::endl;
+            } while (!(y >= 0 && y < 9));
+            do
+            {
+                std::cout << "Enter value: ";
+                std::cin >> value;
+                std::cout << std::endl;
+            } while (!(value >= 0 && value < 9));
+        } while (getCase(x, y) != 0);
         setCase(x, y, value);
     } while (!isSolved());
 }
@@ -196,7 +143,7 @@ void Sudoku::playTheGame()
 bool Sudoku::isSolved()
 {
     bool full = true;
-    for (auto i : _grid)
+    for (auto i : _grid.getGrid())
         if (i == 0)
             return false;
     return checkifSudokuIsCorrect();
@@ -216,6 +163,72 @@ bool Sudoku::checkifSudokuIsCorrect()
         }
     }
     return true;
+}
+
+void Sudoku::swapLines(unsigned int line1, unsigned int line2)
+{
+    for (unsigned int i = 0; i < _size * _size; ++i)
+    {
+        int tmp = getCase(i, line1);
+        setCase(i, line1, getCase(i, line2));
+        setCase(i, line2, tmp);
+    }
+}
+
+void Sudoku::swapColumns(unsigned int column1, unsigned int column2)
+{
+    for (unsigned int i = 0; i < _size * _size; ++i)
+    {
+        int tmp = getCase(column1, i);
+        setCase(column1, i, getCase(column2, i));
+        setCase(column2, i, tmp);
+    }
+}
+
+void Sudoku::shuffleGrid()
+{
+    int sc = 0;
+    int sl = 0;
+    for(int i = 0; i < pow(_size, 4); i++)
+    {
+        int x1 = rand() % (_size * _size);
+        int x2 = rand() % (_size * _size);
+        bool correct = abs(x1 - x2) < _size;
+
+        if(rand() % 1 == 0 && correct)
+        {
+            swapLines(x1, x2);
+            sl++;
+        }
+        else
+        {   
+            swapColumns(x1, x2);
+            sc++;
+        }
+    }
+    std::cout << "swaped lines: " << sl << std::endl;
+    std::cout << "swaped columns: " << sc << std::endl;
+}
+
+bool Sudoku::placeRemarquableNumbers()
+{
+    for(auto it : _grid.getGrid())
+    {
+        if (it == 0)
+        {
+
+        }
+    }
+    return false;
+}
+
+bool Sudoku::solve()
+{
+    do {} while (placeRemarquableNumbers());
+    if (isSolved())
+        return true;
+    else
+        return false;
 }
 
 
